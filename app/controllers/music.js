13c4b681d6611e98e6849a2ed8ac32da187b7361
tsdhr1 <gougewh@sina.com>
2019-11-08 20:11:04 +0800

    :zap:

--- app/controllers/music.js
+++ app/controllers/music.js
@@ -1,1 +1,31 @@
 const Music = require('../models/music');
+const shell = require('shelljs');
+
+class MusicCtl {
+  async updatesong(ctx) {
+    console.log(ctx.req.files.file);
+    
+    ctx.body = {
+      code: 100,
+      data: ctx.req.files.file.path
+    }
+  }
+
+  async updatemusic(ctx) {
+    // const shellCode = await shell.exec(`mv ${ctx.request.body.songPath} /data/music/song`);
+    // if (!shellCode) {
+    //   ctx.body = {
+    //     code: 500,
+    //     msg: '保存文件失败,从重新尝试上传'
+    //   }
+    //   return;
+    // }
+    const songNameArr = ctx.request.body.songPath.split('');
+    console.log(songNameArr[songNameArr.length-1]);
+    
+    ctx.body = '测试是否成功'
+    // ctx.request.body
+  }
+}
+
+module.exports = new MusicCtl()
--- app/routes/music.js
+++ app/routes/music.js
@@ -4,21 +4,23 @@
  */
 const jwt = require('koa-jwt')
 const Router = require('koa-router')
-const { list  } = require('../controllers/users');
 const { secret } = require('../config')
+const multiparty = require('koa2-multiparty');
+
+const { updatesong, updatemusic } = require('../controllers/music');
 
 const router = new Router()
 
 const auth = jwt({ secret })
 
-
-
 const songList = require('../../song-list')
-
-
 
 router.get('/music/songlist',(ctx) => {
     ctx.body = songList
 })
+
+router.post('/music/updatesong', auth, multiparty(), updatesong)
+
+router.post('/music/updatemusic', auth, multiparty(), updatemusic)
 
 module.exports = router
